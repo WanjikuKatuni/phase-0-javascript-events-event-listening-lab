c94b9c20559281f9f58908dbd3cf803aef317432
wanjiku <annritawanjiku77@gmail.com>
2022-08-26 14:01:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 function addingEventListener() {
+    const input = document.getElementById("input")
+
+    function clickAlert(){
+        alert(`WHo clicked me!!!!`)
+    }
+
+    input.addEventListener("click", clickAlert)
 }
+addingEventListener()
+
+
+// const input = document.getElementById("input");
+// input.addEventListener('click',()=> alert(`I was clicked`))
+
+const input = document.getElementById("input")
+
+function clickAlert(){
+    alert(`WHo clicked me!!!!`)
+}
+
+input.addEventListener("click", clickAlert)
